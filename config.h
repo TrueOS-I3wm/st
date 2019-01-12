afdaec8251a9ab8f03969521923bf6a231ff2d38
Martin Wilke <miwi@FreeBSD.org>
2019-01-12 22:19:28 +0800

    - Font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Anonymice Powerline:pixelsize=10:antialias=true:autohint=true";
+static char *font = "Anonymice Powerline:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
